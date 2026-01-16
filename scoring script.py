dbd7bddd1ba5efd593a0f53289d42b7c78dfcb52
NoorMajdoub <nourelhoudamajdoub1@gmail.com>
2026-01-16 01:52:19 +0100

    workflows4

--- scoring script.py
+++ scoring script.py
@@ -5,11 +5,13 @@ import sys
 # Usage : python scoring_script .py submissions / file .csv
 
 submission_file = sys . argv [1]
+truth_file = sys.argv[2]
+
 #Load submission
 submission = pd . read_csv ( submission_file )
 
 12 # Load ground truth ( hidden )
-truth = pd.read_csv('data/test_labels.csv')
+truth = pd.read_csv(truth_file)
 # Compute F1 score
 score = f1_score(truth["label"], submission["predicted_label"], average="macro")
 print ( f'Submission F1 Score : { score :.4f}')
